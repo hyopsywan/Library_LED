7de4cdcfd25220283c33d7cd83f80912ae150544
Heloísa T. <heloisataraujo14@gmail.com>
2026-05-03 17:33:59 -0300

    adicionando exemplo de uso - ligar no tempo

--- examples/01-Acender_led_no_tempo/Acender.cpp
+++ examples/01-Acender_led_no_tempo/Acender.cpp
@@ -1,13 +1,14 @@
-#include <LED.h>
+#include <Arduino.h>
+#include "LED.h"
 
-Led LedBranco(21); //Use a funcao Led() para adicionar em qual pino seu LED esta conectado na placa.
+Led led(13);
 
-void setup() 
+void setup()
 {
-    LedBranco.acender(2); //Use a funcao acender(tempoLigado) para adicionar o tempo em segundos em que o LED ira acender.
+    led.acender(2000);
 }
 
 void loop()
 {
-    LedBranco.update(); //A funcao update deve estar sempre presente no loop.
+    led.update();
 }
